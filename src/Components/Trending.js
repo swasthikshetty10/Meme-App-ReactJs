e49d4d71ebde958c15c5947d84deb19aec048c32
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-04-23 18:46:04 +0530

    CSS IS BAD :(

--- src/Components/Trending.js
+++ src/Components/Trending.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 function Trending() {
     return (
-       
+
         <StoryReel>
             {/* Story */}
             {/* Story */}
--- src/Components/UserPage.js
+++ src/Components/UserPage.js
@@ -3,13 +3,20 @@ import styled from 'styled-components'
 import { Avatar } from '@material-ui/core'
 import { useStateValue } from '../Contexts/StateProvider'
 function UserPage() {
-    const [{user} , dispatch] = useStateValue();
+    const [{ user }, dispatch] = useStateValue();
     console.log(user)
     return (
         <Card>
-            <Avatar src = {user.photoURL} />
-            <h2>Name : { user.displayName}</h2>
-            <h2>Email : { user.email}</h2>
+            <Headder>
+                <div className="Avatar">
+                    <img src={user.photoURL} />
+                </div>
+                <div className="userdata">
+                    <h3>{user.displayName}</h3>
+                    <h2>{user.email}</h2>
+                </div>
+
+            </Headder>
         </Card>
     )
 }
@@ -17,5 +24,20 @@ function UserPage() {
 export default UserPage
 
 const Card = styled.div`
+background-color : white;
+border-radius : 10px;
+
+`
+const Headder = styled.div`
+ display : flex;
+ align-items : center;
+ justify-content : center;
+.Avatar{
+    padding : 10px;
+    img {
+    border-radius : 50%;
+    } 
+    
+}
 `
 
