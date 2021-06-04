ef9e4f2a62ec11aa6c709625a233e8b8e119de1c
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-06-04 20:19:59 +0530

    user Page

--- src/Components/UserPage/index.js
+++ src/Components/UserPage/index.js
@@ -2,38 +2,46 @@ import React from 'react'
 import styled from 'styled-components'
 import { Avatar } from '@material-ui/core'
 import { useStateValue } from '../../Contexts/StateProvider'
+import UserPost from "./UserPost"
 function UserPage() {
     const [{ user }, dispatch] = useStateValue();
     console.log(user)
     return (
-        <Card>
-            <Headder>
-                <div className="Avatar">
-                    <img src={user.photoURL} />
-                </div>
-                <div className="userdata">
-                    <h3>{user.displayName}</h3>
-                    <h2>{user.email}</h2>
+        <>
+            <Card className="bg-white rounded-xl shadow-md my-2">
+                <Headder className=" flex  justify-center gap-2 md:gap-10">
+                    <div className="Avatar my-4 ">
+                        <img src={user.photoURL} />
+                    </div>
+                    <div className="userdata">
+                        <div className="text-2xl flex gap-2 items-center"><p>{user.displayName}</p><button className="px-2 py-1 text-sm  bg-blue-500 outline-0 font-bold text-white rounded rounded-md">Follow</button></div>
+                        <h2 className="text-gray-700">{user.email}</h2>
+                        <div className="flex justify-start gap-2 md:gap-5"><p><span class="font-semibold" >0</span> followers </p> <p><span class="font-semibold" >0</span> Posts</p></div>
+                    </div>
+
+                </Headder>
+                <div className="flex justify-center">
+
+
                 </div>
 
-            </Headder>
-        </Card>
+            </Card>
+            <UserPost />
+        </>
     )
 }
 
 export default UserPage
 
 const Card = styled.div`
-background-color : white;
-border-radius : 10px;
+
 
 `
 const Headder = styled.div`
- display : flex;
  align-items : center;
- justify-content : center;
+
 .Avatar{
-    padding : 10px;
+   
     img {
     border-radius : 50%;
     } 
